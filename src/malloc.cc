1a64491d13582b38f2fcca706c8c824e1b85fb82
Aditya Madhan <adityamkk@gmail.com>
2026-05-07 22:36:48 -0500

    feat: kmalloc updated to use footers (todo: update free)

--- src/malloc.cc
+++ src/malloc.cc
@@ -2,54 +2,67 @@
 #include <stddef.h>
 
 #define HEAP_SIZE 8 * 1024 * 1024
+#define HEAP_AL 16
 
 char heap[HEAP_SIZE];
 
-struct free_list_header {
-  size_t size; // includes the size of the header
+struct alignas(HEAP_AL) free_list_header {
+  size_t size; // Includes the size of the header and footer
   bool used;
 };
 
-const size_t MIN_FREE_BLOCK_SIZE_DATA = 2*sizeof(free_list_header); // Does not include the size of the header
+struct alignas(HEAP_AL) free_list_footer {
+  size_t size; // Must always be the same size as the corresponding header
+};
+
+// Does not include the size of the header or footer
+const size_t MIN_FREE_BLOCK_SIZE_DATA = sizeof(free_list_header);
 
 void init_malloc() {
   free_list_header* first_header = reinterpret_cast<free_list_header*>(heap);
   first_header->size = HEAP_SIZE;
   first_header->used = false;
+  free_list_footer* first_footer = reinterpret_cast<free_list_footer*>(heap + HEAP_SIZE - sizeof(free_list_footer));
+  first_footer->size = HEAP_SIZE;
 }
 
 /**
- * Allocates a specified number of bytes for kernel use, aligned to a specified amount of bytes.
+ * Allocates a specified number of bytes for kernel use.
  * ONLY VALID FOR SINGLE CORE
- * @param size      The number of bytes to allocate within the kernel heap
- * @param al        The number of bytes to align the returned address to.
- * @returns         A pointer to an allocated region of the kernel heap. (Guaranteed to be divisible by al)
+ * @param size_      The number of bytes to allocate within the kernel heap
+ * @returns         A pointer to an allocated region of the kernel heap. (Guaranteed to be divisible by HEAP_AL)
  */
-void* kmalloc(size_t size, size_t al) {
+void* kmalloc(size_t size_) {
+  size_t size = ((size_ + HEAP_AL - 1) / HEAP_AL) * HEAP_AL; // Round up to the nearest multiple of HEAP_AL
   free_list_header *current = reinterpret_cast<free_list_header*>(heap);
   while(current < reinterpret_cast<free_list_header*>(heap) + HEAP_SIZE) {
     // find the first aligned address within this block 
     // current design deals with alignment requirements by just wasting the first part of any block wr find
     size_t base_addr = (size_t) current + sizeof(free_list_header);
-    size_t al_offset = al - (base_addr % al);
-    if (current->used || (al_offset + size > current->size - sizeof(free_list_header))) {
+    if (current->used || (size > current->size - sizeof(free_list_header) - sizeof(free_list_footer))) {
       current = current + current->size; // Move to the next header
       continue;
     }
-    // First-fit policy, carve out this region and mark it as used
-    size_t aligned_addr = base_addr + al_offset;
-    size_t old_size = current->size;
+    // First-fit policy
     current->used = true;
 
     // Check if there is enough room to make a new free list block
-    if (old_size - (sizeof(free_list_header) + al_offset + size) > sizeof(free_list_header) + MIN_FREE_BLOCK_SIZE_DATA) {
-      // Insert a new free list header
-      current->size = sizeof(free_list_header) + al_offset + size; // Carve out reserved portion
+    if (current->size - (sizeof(free_list_header) + size + sizeof(free_list_footer)) > sizeof(free_list_header) + MIN_FREE_BLOCK_SIZE_DATA + sizeof(free_list_footer)) {
+      // Carve out the reserved, mallocced portion
+      size_t old_size = current->size;
+      current->size = sizeof(free_list_header) + size + sizeof(free_list_footer);
+      // Update the footer
+      free_list_footer *current_footer = reinterpret_cast<free_list_footer*>(current + current->size - sizeof(free_list_footer));
+      current_footer->size = current->size;
+      // Add the new free block
       free_list_header *post = reinterpret_cast<free_list_header*>(current + current->size); 
       post->size = old_size - current->size;
       post->used = false;
+      // Update the post footer
+      free_list_footer *post_footer = reinterpret_cast<free_list_footer*>(post + post->size - sizeof(free_list_footer));
+      post_footer->size = post->size;
     }
-    return reinterpret_cast<void*>(aligned_addr);
+    return reinterpret_cast<void*>(base_addr);
   }
   // TODO: We need a kernel panic here
   return nullptr;
