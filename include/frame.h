a0904558c47657950b43bcdad3fa0ea1f2f71f61
Aditya Madhan <adityamkk@gmail.com>
2026-05-12 11:48:43 -0500

    fix: exposed address translation api

--- include/frame.h
+++ include/frame.h
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #define PAGE_SIZE 4096
 
+extern inline uint64_t pa2va(uint64_t pa);
+extern inline uint64_t va2pa(uint64_t va);
+
 extern void init_falloc(uint64_t hhdm_offset, struct limine_memmap_response* memmap_response);
 extern void* falloc();
 extern void ffree(void* frame);
